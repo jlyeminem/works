403cf6c60669d019ca58f91c518a9681ea74b858
jeminem <97281585@qq.com>
2016-09-21 21:04:07 +0800

    [jly] 完成项目的搭建以及创建时间解析及其测试

--- src/main/java/utils/TimeUtils.java
+++ src/main/java/utils/TimeUtils.java
@@ -6,19 +6,19 @@ import java.util.Date;
 
 public class TimeUtils {
 
-    public static String dateToStamp(String string) throws ParseException {
+    public static String dateToStamp(String str) throws ParseException {
         String res;
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date = simpleDateFormat.parse(string);
+        Date date = simpleDateFormat.parse(str);
         long ts = date.getTime();
         res = String.valueOf(ts);
         return res;
     }
 
-    public static String stampToDate(String string) {
+    public static String stampToDate(String str) {
         String res;
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        long lt = new Long(string);
+        long lt = new Long(str);
         Date date = new Date(lt);
         res = simpleDateFormat.format(date);
         return res;
